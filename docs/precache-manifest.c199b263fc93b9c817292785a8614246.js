1f7e6077ffa3d08fd8686403de73ce411e482405
Ariel Chura <lf0ariel@gmail.com>
2020-12-23 17:06:56 -0400

    Cambios y actualizaciones en docs

---
+++ docs/precache-manifest.c199b263fc93b9c817292785a8614246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494fa07a810f2d178e5c8e5954a4d4ee",
+    "url": "/react-GifApp/index.html"
+  },
+  {
+    "revision": "98614633127fd2e461af",
+    "url": "/react-GifApp/static/css/main.f4550af2.chunk.css"
+  },
+  {
+    "revision": "ce81963eb40ccd372096",
+    "url": "/react-GifApp/static/js/2.95c7aa86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-GifApp/static/js/2.95c7aa86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98614633127fd2e461af",
+    "url": "/react-GifApp/static/js/main.a77d7cc6.chunk.js"
+  },
+  {
+    "revision": "e37d4786182ee6857d4e",
+    "url": "/react-GifApp/static/js/runtime-main.bb00aa6f.js"
+  }
+]);
